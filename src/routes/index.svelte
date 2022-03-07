043325911e35de22b217c5aedcf6f1e9fed4eba6
John <john.bow7720@gmail.com>
2022-03-06 21:59:21 -0700

    wip: getting the UI going, hitting API

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import type { NamedAPIResource, NamedAPIResourceList } from '../lib/types';
+
+	let pokemonList: NamedAPIResource[] | null;
+
+	onMount(async (): Promise<void> => {
+		const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+		const jsonRes = await res.json();
+		pokemonList = jsonRes.results;
+	});
+</script>
+
+<h2>All Pokémon</h2>
+<input type="text" placeholder="Search" />
+{#if pokemonList}
+	<ul>
+		{#each pokemonList as pokemon, i}
+			<li>{i + 1}: {pokemon.name}</li>
+		{/each}
+	</ul>
+{/if}
