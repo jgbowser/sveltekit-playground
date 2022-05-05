8eba9dfeca70010398052e76f403e51fd0221384
John <john.bow7720@gmail.com>
2022-05-04 19:36:28 -0600

    roughing out the pokemon card component, formatting changes

---
+++ src/lib/components/PokemonCard.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { NamedAPIResource } from '$lib/types';
+	export let pokemon: NamedAPIResource;
+	export let index: number;
+</script>
+
+<div>
+	<img src="./pikachu-img.png" alt="{pokemon.name} sprite" />
+	<div>
+		<p><span>{index + 1}. </span>{pokemon.name}</p>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	// import { onMount } from 'svelte';
+	import { pokemonData } from '$lib/exampleData';
+	import PokemonCard from '$lib/components/PokemonCard.svelte';
 
 	import type { NamedAPIResource, NamedAPIResourceList } from '../lib/types';
 
 	let pokemonList: NamedAPIResource[] | null;
 
-	onMount(async (): Promise<void> => {
-		const res = await fetch('https://pokeapi.co/api/v2/pokemon');
-		const jsonRes = await res.json();
-		pokemonList = jsonRes.results;
-	});
+	// onMount(async (): Promise<void> => {
+	// 	const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+	// 	const jsonRes = await res.json();
+	// 	pokemonList = jsonRes.results;
+	// });
 </script>
 
 <h2>All Pokémon</h2>
 <input type="text" placeholder="Search" />
-{#if pokemonList}
+{#if pokemonData.results}
 	<ul>
-		{#each pokemonList as pokemon, i}
-			<li>{i + 1}: {pokemon.name}</li>
+		{#each pokemonData.results as pokemon, index}
+			<PokemonCard {index} {pokemon} />
 		{/each}
 	</ul>
 {/if}
